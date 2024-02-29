7c8ad56b64f480f76422f9ffd5a7b66daf9161c5
Alexandr Morozov <morozov76@gmail.com>
2024-02-29 11:32:07 +0600

    Added first task

--- Semi1/Program.cs
+++ Semi1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход
+принимает два целых числа и проверяет,
+является ли первое число квадратом второго*/
+
+Console.Write("input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
