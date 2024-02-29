f77ecef982b790bc024decbd6f31633e64366de9
Alexandr Morozov <morozov76@gmail.com>
2024-02-29 18:35:36 +0600

    Solved task nuber three

--- Semi1/Task3/Program.cs
+++ Semi1/Task3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+
+Console.Write("Input your three digit number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a <= 999 && a >= 100)
+{
+    int b = a / 100;
+    int c = a % 10;
+    int sum = b + c;
+    Console.WriteLine(sum);
+    Console.WriteLine(a / 10 % 10);
+}
+else
+{
+    Console.WriteLine("Your number is not three digit");
+}
