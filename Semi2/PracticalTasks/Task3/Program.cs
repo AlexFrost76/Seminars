9d3ee2ff809693a2c5055e6e47d58b5c22f77409
Alexandr Morozov <morozov76@gmail.com>
2024-03-07 18:14:29 +0600

    Task3 solved in the Seminar2

--- Semi2/PracticalTasks/Task3/Program.cs
+++ Semi2/PracticalTasks/Task3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program{
+    static void Main()
+    {
+        Console.Write("ВВедите координаты точки X и Y через пробел: ");
+        string[] coordinates = Console.ReadLine().Split(" ");
+        int x = Convert.ToInt32(coordinates[0]);
+        int y = Convert.ToInt32(coordinates[1]);
+
+        if (x > 0 && y > 0)
+            Console.WriteLine("1");
+        else if (x < 0 && y > 0)
+            Console.WriteLine("2");
+        else if (x < 0 && y < 0)
+            Console.WriteLine("3");
+        else if (x > 0 && y < 0)
+            Console.WriteLine("4");
+        else
+        Console.WriteLine("Точка находится на оси координат");
+    }
+}
