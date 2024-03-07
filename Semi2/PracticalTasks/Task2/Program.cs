5316d2333864fdfa5d8020a857ea7074a458b179
Alexandr Morozov <morozov76@gmail.com>
2024-03-07 17:52:16 +0600

    Added Task3 to the Seminar2

--- Semi2/PracticalTasks/Task2/Program.cs
+++ Semi2/PracticalTasks/Task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if(a % 7 == 0 && a% 23 == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else 
+        {
+            Console.WriteLine("NO");
+        }
+    }
+
+}
--- Semi2/Task1/Program.cs
+++ Semi2/Task1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Bar(ref int a)
+{
+    a =0;
+}
+
+int value =5;
+ Bar( ref value);
+Console.WriteLine(value);
