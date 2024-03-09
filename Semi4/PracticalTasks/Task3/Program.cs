3d7e0a3a79810ca90503cb42913830710ae3ec3f
Alexandr Morozov <morozov76@gmail.com>
2024-03-09 21:43:25 +0600

    Resolve Task3

---
+++ Semi4/PracticalTasks/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 }; // Source array
+        int temp;
+        // Output of the original array
+        Console.Write("Original array: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        // Array reversal
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            // Swap elements
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+        // Вывод измененного массива
+        Console.Write("\nInverted array: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
