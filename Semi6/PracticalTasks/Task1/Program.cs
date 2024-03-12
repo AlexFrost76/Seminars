b153325fc1e865660b840c29d8ba7b7061b29c32
Alexandr Morozov <morozov76@gmail.com>
2024-03-12 16:41:10 +0600

    Resolve Semi6:Task1

--- Semi6/PracticalTasks/Task1/Program.cs
+++ Semi6/PracticalTasks/Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Задайте двумерный массив символов (тип char [,]). 
+//Создать строку из символов этого массива.
+using System;
+class Program
+{
+    static void Main()
+    {
+        char[,] CharArray = new char[,]{{'H','e','l','l','o'},{'W','o','r','l','d'}};
+        //Console.WriteLine($"CharArray rows {CharArray.GetLength(0)}");
+        //Console.WriteLine($"CharArray cols {CharArray.GetLength(1)}");
+        
+         string str = "";
+         for (int a = 0; a < CharArray.GetLength(0); a++ )
+         {
+             for (int b = 0; b < CharArray.GetLength(1); b++)
+             {
+                str = str + CharArray[a,b].ToString();
+             }
+         }
+         Console.WriteLine(str);
+    }
+}
