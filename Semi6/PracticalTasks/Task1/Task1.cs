16d7aa8b1cd96986dbe252d609fdf70f6a04c885
Alexandr Morozov <morozov76@gmail.com>
2024-03-12 21:50:05 +0600

    Rename Program.cs to Task..

---
+++ Semi6/PracticalTasks/Task1/Task1.cs
@@ -0,0 +1,27 @@
+﻿//Задача 1: Задайте двумерный массив символов (тип char [,]). 
+//Создать строку из символов этого массива.
+using System;
+class Program
+{
+    static void Main()
+    {
+        char[,] CharArray = new char[,]{{'H','e','l','l','o'},{'W','o','r','l','d'}};
+        //Console.WriteLine($"CharArray rows {CharArray.GetLength(0)}");
+        //Console.WriteLine($"CharArray cols {CharArray.GetLength(1)}");
+        
+         string str = StringFromArray(CharArray);
+         Console.WriteLine(str);
+    }
+    public static string StringFromArray(char[,] CharArray)
+    {
+        string str = "";
+         for (int a = 0; a < CharArray.GetLength(0); a++ )
+         {
+             for (int b = 0; b < CharArray.GetLength(1); b++)
+             {
+                str = str + CharArray[a,b].ToString();
+             }
+         }
+         return str;
+    }
+}
