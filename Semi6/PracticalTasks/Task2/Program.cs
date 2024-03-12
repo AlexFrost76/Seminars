028817fb7f41ccea6df2c721daf5a8ba62ae1685
Alexandr Morozov <morozov76@gmail.com>
2024-03-12 16:54:57 +0600

    Resolve Semi6:Task2

--- Semi6/PracticalTasks/Task2/Program.cs
+++ Semi6/PracticalTasks/Task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+using System;
+class Program{
+    static void Main()
+    {
+        string str = "HeLlo WorLd";
+        str = str.ToLower();
+        Console.WriteLine(str);
+    }
+}
