b4a56f8008dc7bbce47ef2bd6a934baf33dfea90
Alexandr Morozov <morozov76@gmail.com>
2024-03-13 22:42:30 +0600

    Solved Semi7:Task1

---
+++ Semi7/PracticalTasks/Task1/Task1.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+using System;
+class Task1
+{
+    public static void Main()
+    {
+        Console.Write("Введите значение M:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        PrintAllNaturalNumbers(m,n);
+    }
+    public static void PrintAllNaturalNumbers(int m, int n)
+    {
+        if (m > n)
+            return;
+       
+        Console.Write($"{m} ");
+        PrintAllNaturalNumbers(m + 1, n);
+
+    }
+}
