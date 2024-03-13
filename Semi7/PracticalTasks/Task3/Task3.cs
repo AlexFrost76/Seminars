df0203fd068305c8823ad4b3a0f2dbb3f7ff8ef7
Alexandr Morozov <morozov76@gmail.com>
2024-03-14 00:32:53 +0600

    Resolve Semi7:Task3

--- Semi7/PracticalTasks/Task3/Task3.cs
+++ Semi7/PracticalTasks/Task3/Task3.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+
+public static class Program
+{
+    public static void ReverseArr(int[] arr, int n)
+    {
+        if (n > 0)
+        {
+            Console.Write(arr[n] + " ");
+            ReverseArr(arr, n - 1);
+        }
+        else Console.WriteLine(arr[0]);
+    }
+    public static void Main()
+    {
+        int[] arr = { 1, 2, 5, 0, 10, 34 };
+        ReverseArr(arr, arr.Length - 1);
+    }
+}
